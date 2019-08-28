159002fd0129a62b82d065675b90526141bac86f
elgine <624157383@qq.com>
2019-08-28 08:50:19 +0800

    commit test case

--- test/matrix.test.ts
+++ test/matrix.test.ts
@@ -53,5 +53,44 @@ describe('matrix & pixi-matrix', () => {
 
     test('decompose', () => {
 
+        const tx = 20;
+        const ty = 20;
+        const sx = 1.2;
+        const sy = 2;
+        const r = Math.PI / 3;
+        const skx = 0;
+        const sky = 0;
+
+        const pixiMat = new Matrix();
+        pixiMat.setTransform(
+            tx, ty,
+            0, 0,
+            sx, sy,
+            r,
+            skx, sky
+        );
+
+        const transform = {
+            translation: [0, 0],
+            scale: [1, 1],
+            rotation: 0,
+            skew: [0, 0]
+        };
+        const matrix = MyMatrix.create();
+        MyMatrix.recompose({
+            translation: [tx, ty],
+            scale: [sx, sy],
+            rotation: r,
+            skew: [skx, sky]
+        }, [0, 0], matrix);
+        MyMatrix.decompose(matrix, transform);
+
+        expect(transform.translation[0]).toEqual(tx);
+        expect(transform.translation[1]).toEqual(ty);
+        expect(transform.scale[0]).toEqual(sx);
+        expect(transform.scale[1]).toEqual(sy);
+        expect(transform.rotation).toEqual(r);
+        expect(transform.skew[0]).toEqual(skx);
+        expect(transform.skew[1]).toEqual(sky);
     });
 });
